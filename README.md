cb210b7feced702fc5b61f40158898aa7db5e067
Viktor <viktorziegler99@live.de>
2022-04-29 21:49:42 +0200

    init

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # eftCashBot
+
+Simple code for a discord bot for the Lightkeeper Cash Event in Escape from Tarkov. 
+
+# Setup
+
+To get started create a discord bot and replace TOKEN with your bot token. 
+Invite your bot to a server and write `/startCash` in a text channel to start the bot. Stop the bot with `/stopCash`
---
+++ src/eft_cash_bot.py
@@ -0,0 +1,55 @@
+import asyncio
+import discord
+import requests
+import re
+
+
+async def get_cash(message):
+    last_value = 0
+
+    while True:
+        page_content = requests.get(url='https://www.escapefromtarkov.com/cash').text
+        matching_content = re.compile("var x = (\\d+),", re.MULTILINE).findall(page_content)
+        new_value = int(matching_content.pop())
+        if last_value != new_value:
+            diff = new_value - last_value
+            percentage = new_value / 1500000000000
+            if last_value != 0:
+                try:
+                    await message.channel.send(
+                        "Current: {} Diff: {} Progress: {}".format(f"{new_value:,}", f"{diff:,}", f"{percentage:.2%}"))
+                except Exception:
+                    print("Something went wrong in channel {}", message.channel)
+
+            last_value = new_value
+        await asyncio.sleep(5)
+
+
+class EftCashBot(discord.Client):
+    running = False
+
+    async def on_message(self, message):
+        if message.author == client.user:
+            return
+        if message.content == "/startCash" and self.running is False:
+            print("start in {}".format(message.channel))
+            self.running = True
+            asyncio.get_event_loop().create_task(get_cash(message))
+            try:
+                await message.channel.send("Waiting for cash updates")
+            except Exception:
+                print("Something went wrong in channel {}", message.channel)
+
+        if message.content == "/stopCash" and self.running:
+            print("stop in {}".format(message.channel))
+            self.running = False
+            try:
+                await message.channel.send("Stopped the cash updates")
+            except Exception:
+                print("Something went wrong in channel {}", message.channel)
+            asyncio.get_event_loop().stop()
+
+
+intents = discord.Intents.default()
+client = EftCashBot(intents=intents)
+client.run('TOKEN')
