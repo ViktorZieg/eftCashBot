7b611c909919be126182faf72368312cf4b90321
Viktor <viktorziegler99@live.de>
2022-04-30 11:15:24 +0200

    adjusted bot

--- src/eft_cash_bot.py
+++ src/eft_cash_bot.py
@@ -25,31 +25,39 @@ async def get_cash(message):
         await asyncio.sleep(5)
 
 
+def get_task_id(message):
+    return message.guild.id + message.channel.id
+
+
 class EftCashBot(discord.Client):
-    running = False
+    tasks = {}
 
     async def on_message(self, message):
         if message.author == client.user:
             return
-        if message.content == "startEftCash" and self.running is False:
-            print("start in {}".format(message.channel))
-            self.running = True
-            asyncio.get_event_loop().create_task(get_cash(message))
-            try:
+        if message.content == "startEftCash":
+            task_id = get_task_id(message)
+            if task_id in self.tasks:
+                await message.channel.send("You already have a bot in this channel")
+                print("{} tried to duplicate".format(task_id))
+            else:
+                print("Start id {} Server: {} Channel: {}".format(task_id, message.guild.name, message.channel.name))
+                new_task = self.loop.create_task(get_cash(message))
+                self.tasks[task_id] = new_task
                 await message.channel.send("Waiting for cash updates")
-            except Exception:
-                print("Something went wrong in channel {}", message.channel)
 
-        if message.content == "stopEftCash" and self.running:
-            print("stop in {}".format(message.channel))
-            self.running = False
-            try:
+        if message.content == "stopEftCash":
+            task_id = get_task_id(message)
+            if task_id in self.tasks:
+                task = self.tasks[task_id]
+                print("Stop id {} Server: {} Channel: {}".format(task_id, message.guild.name, message.channel.name))
+                task.cancel()
+                del self.tasks[task_id]
                 await message.channel.send("Stopped the cash updates")
-            except Exception:
-                print("Something went wrong in channel {}", message.channel)
-            asyncio.get_event_loop().stop()
+            else:
+                await message.channel.send("There is no bot started")
 
 
-intents = discord.Intents.default()
-client = EftCashBot(intents=intents)
+intents = discord.Intents.all()
+client = EftCashBot(intents=intents, loop=asyncio.new_event_loop())
 client.run('TOKEN')
